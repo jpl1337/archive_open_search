d09c54f7462d0ee3923d43a4a2edda68a976e9e3
Jeremy Pena-Lopez <6828800+jpl1337@users.noreply.github.com>
2025-03-22 10:32:55 -0400

    added missing function definition in page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,17 +2,22 @@ class Page < ApplicationRecord
     belongs_to :document
 
     after_save :update_document_status!
-    pages = document.pages
-    if pages.all? { |page| page.status == "completed"}
-        document.update!(status: "completed")
-    elsif pages.any? {|page| page.status == "error"}
-        document.update!(status: "partial_error")
-    elsif pages.any? {|page| page.status == "processing"}
-        document.upate!(status: "processing")
-    elsif pages.all? {|page| page.status == "error"}
-        document.update!(status: "error")
-    else
-        document.upate!(status: "pending")
-    validates :page_number, presence: true
-    validates :status, inclusion: {in: %w[pending processing completed error] }
+    
+    def update_document_status!
+        pages = document.pages
+
+        if pages.all? { |page| page.status == "completed"}
+            document.update!(status: "completed")
+        elsif pages.any? {|page| page.status == "error"}
+            document.update!(status: "partial_error")
+        elsif pages.any? {|page| page.status == "processing"}
+            document.upate!(status: "processing")
+        elsif pages.all? {|page| page.status == "error"}
+            document.update!(status: "error")
+        else
+            document.upate!(status: "pending")
+        validates :page_number, presence: true
+        validates :status, inclusion: {in: %w[pending processing completed error] }
+        end
+    end
 end
